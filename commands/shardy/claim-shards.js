d85c64bf14b1eff733510bcb1f31f58b1d5be2d6
SebestikCZ <44745014+SebestikCZ@users.noreply.github.com>
2020-12-02 20:24:01 +0100

    Update claim-shards.js

--- commands/shardy/claim-shards.js
+++ commands/shardy/claim-shards.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js")
-const db = require("quick.db")
+const Discord = require("discord.js");
+const db = require("quick.db");
 
 
 module.exports = {
@@ -7,25 +7,28 @@ module.exports = {
     category: "shards",
     run: async(client, message, args) => {
         message.delete()
-        let member = message.author
+        let user = message.author
         let claimamount = "50"
-        let claimedRole = message.member.roles.cache.find(r => r.name === "Claimed")
+        //let claimedRole = message.member.roles.cache.find(r => r.name === "Claimed")
 
-        if (claimedRole) {
+        const isClaimed = db.get(`${user.id}.claimed`);
+        if (isClaimed) {
             let claimed = new Discord.MessageEmbed()
                 .setColor("RED")
-                .setTitle(`**${member.username}**, svoji vánoční odměnu si **už vyzvednul!**`)
+				.setAuthor(user.tag, user.avatarURL({dynamic: true}), "http://eventverse.eu")
+                .setTitle(`Svoji vánoční odměnu si **již vyzvednul!**`);
 
             message.channel.send(claimed)
         } else {
             let embed = new Discord.MessageEmbed()
                 .setTitle("<:pepeSanta:782344286896259143> | **Vánočný claim**")
-                .addField(`${member.username}, jako dárek jsme ti připsali **50** <:EV_Shard:776100019907592192>`, "Tento příkaz můžeš použiť **jenom jednou!**")
-                .setFooter("Veselé vánoce přeje tým EventVerse", member.avatarURL({dynamic:"gif"}))
-                .setColor(0xad0205)
-
-                message.member.roles.add("780925492831715379");
-            db.add(`shards_${member.id}-${message.guild.id}`,`${claimamount}`)
+                .addField(`${user.username}, jako dárek jsme ti připsali **50** <:EV_Shard:776100019907592192>`, "Tento příkaz můžeš použiť **jenom jednou!**")
+                .setFooter("Veselé vánoce přeje tým EventVerse", user.avatarURL({dynamic: true}))
+				.setTimestamp()
+                .setColor(0xad0205);
+                db.set(`${user.id}.claimed`, true);
+                //message.member.roles.add("780925492831715379");
+            db.add(`shards_${user.id}-${message.guild.id}`,`${claimamount}`)
             message.channel.send(embed)
         }
     }
