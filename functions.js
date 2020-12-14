54e853d8f7f6501af9dd439d0cb22c2787ff70a5
SebestikCZ <44745014+SebestikCZ@users.noreply.github.com>
2020-12-14 22:42:32 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,2 +1,10 @@
 module.exports = {
-    verifyOverdrawn(message, amount)
+    async function verifyOverdrawn(message, amount) {
+        const db = require('quick.db');
+        let member = message.mentions.users.first() || message.guild.members.cache.get(args[0]).user;
+        let author = message.author;
+        let shards = db.get(`shards_${author.id}-${message.guild.id}`);
+        if (amount > shards) return false;
+        else return true;
+    }
+}
